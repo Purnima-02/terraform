553d0b138bffd64dbb8781952bde4bb2bcb8dbc6
EC2 Default User <ec2-user@ip-172-31-1-154.ap-south-1.compute.internal>
2023-10-20 06:05:45 +0000

    the computer

---
+++ readme.md
@@ -0,0 +1,2 @@
+jnskjdcsdvcksdnvsdisd, ksdvlsdlk lsd l losdlsm sdvslsd sdjvod bb cdscdvdvdfvfd xc zxcdxvdxvdxv dvvddvdvdvdsvd cdscsdcSDCScs
+
---
+++ vpc.tf
@@ -0,0 +1,10 @@
+#creating vpc.tf
+resource "aws_vpc" "demovpc" {
+cidr_block= "10.0.0.0/16"
+instance_tenancy = "default"
+tags = {
+Name="demovpc"
+}
+}
+~
+
